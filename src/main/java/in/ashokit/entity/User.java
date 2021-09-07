aaa0b7792299374f5debf942f4519bbdaa92be05
DESKTOP-F4RNNA9\Sagar <sagarkmohapatra2012@gmail.com>
2021-09-07 09:30:57 -0700

    user entity class added

--- src/main/java/in/ashokit/entity/User.java
+++ src/main/java/in/ashokit/entity/User.java
@@ -1,8 +1,52 @@
 package in.ashokit.entity;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.Data;
 
 @Entity
+@Table(name = "user")
+@Data
 public class User {
+	@Id
+	@GeneratedValue
+	@Column(name = "user_id")
+	private Integer userId;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	private String email;
+	@Column(name = "ph_no")
+
+	private String phNo;
+
+	private String dob;
+	private String gender;
+	private String country;
+	private String state;
+	private String city;
+
+	@CreationTimestamp
+	@Column(name = "created_date", updatable = false)
+	private LocalDate createdDate;
+
+	@UpdateTimestamp
+	@Column(name = "update_date", insertable = false)
+	private LocalDate updatedDate;
+
+	private String password;
 
 }
