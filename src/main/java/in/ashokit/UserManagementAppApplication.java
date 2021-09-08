eddcaf5c5c64a456a267284055f40a2fcc5d2ae8
DESKTOP-F4RNNA9\Sagar <sagarkmohapatra2012@gmail.com>
2021-09-07 23:20:30 -0700

    spaces removed from context path

--- src/main/java/in/ashokit/UserManagementAppApplication.java
+++ src/main/java/in/ashokit/UserManagementAppApplication.java
@@ -2,29 +2,13 @@ package in.ashokit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import in.ashokit.controller.UserController;
 
 @SpringBootApplication
 public class UserManagementAppApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(UserManagementAppApplication.class, args);
-//		UserController userController = context.getBean(UserController.class);
-//		User user = new User();
-//		user.setFirstName("sagar");
-//		user.setLastName("mahapatra");
-//		user.setEmail("sagar@gmail.com");
-//
-//		String userRegistration = userController.UserRegistration(user);
-//		System.out.println(userRegistration);
-//		String msg = userController.unlockAccount("sagar@gmail.com", "dummy", "newpsaword");
-//		System.out.println(msg);
-//		String userLogin = userController.userLogin("sagar@gmail.com", "newpsaword");
-//		System.out.println(userLogin);
-//		String msg2 = userController.forgotPassword("sagar@gmail.com");
-//		System.out.println(msg2);
+		SpringApplication.run(UserManagementAppApplication.class, args);
+
 	}
 
 }
