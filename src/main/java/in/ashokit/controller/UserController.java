8b0b87febcb915d21df37ba4957cc3178ce468a3
DESKTOP-F4RNNA9\Sagar <sagarkmohapatra2012@gmail.com>
2021-09-08 23:22:42 -0700

    user registration page added with jQuery form validation

--- src/main/java/in/ashokit/controller/UserController.java
+++ src/main/java/in/ashokit/controller/UserController.java
@@ -1,6 +1,7 @@
 package in.ashokit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,11 @@ public class UserController {
 
 	@Autowired
 	private UserService service;
+
+	@GetMapping("/userReg")
+	public String loadRegistrationForm() {
+		return "userRegistration";
+	}
 
 	@PostMapping("/register")
 	public String UserRegistration(@RequestBody User user) {
