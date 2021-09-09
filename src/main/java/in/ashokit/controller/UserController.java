398248942ea39964abed8e57dd418d3996f19b3f
DESKTOP-F4RNNA9\Sagar <sagarkmohapatra2012@gmail.com>
2021-09-09 00:56:01 -0700

    reg page modified

--- src/main/java/in/ashokit/controller/UserController.java
+++ src/main/java/in/ashokit/controller/UserController.java
@@ -1,24 +1,24 @@
 package in.ashokit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import in.ashokit.entity.User;
 import in.ashokit.service.UserService;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
 
 	@Autowired
 	private UserService service;
 
-	@GetMapping("/userReg")
+	@GetMapping("/loadRegForm")
 	public String loadRegistrationForm() {
 		return "userRegistration";
 	}
